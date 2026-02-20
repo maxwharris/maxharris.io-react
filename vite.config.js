91507842e9972ad6a827c00c0d55c508b7418214
max <maxwilliamharris@gmail.com>
2026-02-20 17:07:06 -0500

    updated config and vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names?.[0]?.endsWith('.mjs')) {
+            return 'assets/[name]-[hash].js'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  },
   server: {
     host: true,
     port: 5173,
